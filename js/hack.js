593eb6072d0748f775e42d967bd0908da9bd6bd9
OLU DAVID <olu1995@hotmail.com>
2016-04-09 21:05:25 -0700

    update

--- js/hack.js
+++ js/hack.js
@@ -1,77 +1,64 @@
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.menu').click(function() {
-  	$('.menu').toggleClass('menu-close');
-  	$('.fa-bars').toggleClass("fa-times");
+var main = function () {
+   /*var inter = setInterval(ranpos, 1000); 
 
-    $('.nav-list').animate({
-      right: "0px"
-    }, 500);
-
-    $('body').animate({
-      right: "285px"
-    }, 500);
-  });
-
-  /* Then push them back */
-  $('.menu-close').click(function() {
-  	$('.menu-close').toggleClass('menu');
-  	$('.fa-times').toggleClass("fa-bars");
-    $('.nav-list').animate({
-      right: "-285px"
-    }, 200);
-
-    $('body').animate({
-      right: "0px"
-    }, 200);
-  });
-  
-  $(document).scroll(function () { 
-      var y = $(this).scrollTop();        
-      if (y > 100) {
-          $('.navi').css({"background-color":"white", "color":"#333"});
-          $('.menu-close').css("color", "white");
-      }
-      else {
-          $('.navi').css({"background-color":"transparent", "color":"white"}); 
-      }
-      if (y > 600 && y < 2700) {
-        $('.top').fadeIn('slow');
-      }
-      else {
-        $('.top').fadeOut('slow');
-      }
-  });
-
-  $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+   $.each($('.header span i'), function () {
+      $('.header span i').css({
+          "left": randomwidth,
+          "top": randomheight,
+          "color": "white"
+      }).animate({
+        "top": "50px"
+      }, 5000);
+   })*/
+   $('.header h1').fadeIn(1500).animate({
+      "bottom": "-100px"
+   }, 1000).css("color", "white");
+   setTimeout(function() {
+    clearInterval(inter)
+   }, 10000);
+/*
+  $(document).scroll(function () {
+  var st = $(this).scrollTop();
+     if (st > 100){
+            $('.navbar-nav li a').addClass("animated");
+            $('.navbar-nav li a').addClass("fadeOutRight");
+            $('.navbar').fadeOut(300);
+        }
+    else {
+          $('.navbar').fadeIn(200);
+          $('.navbar-nav li a').removeClass("fadeOutRight");
+          $('.navbar-nav li a').addClass("fadeInRight");
     }
-  });
-});
-
-var lastScrollTop = 0;
+  })
+   */
+   var lastScrollTop = 0;
 
 window.addEventListener("scroll", function(){ 
    var st = window.pageYOffset || document.documentElement.scrollTop; 
-   if (st > 500){
-      if (st > lastScrollTop){
-         $('.navi').css("opacity", "0");
-        
-     } else {
-        $('.navi').css("opacity", "1");
-     }
-  }
+   if (st > lastScrollTop){
+            $('.navbar-nav li a').addClass("animated");
+            $('.navbar-nav li a').addClass("fadeOutRight");
+            $('.navbar').fadeOut(300);
+        }
+    else {
+          $('.navbar').fadeIn(200);
+          $('.navbar-nav li a').removeClass("fadeOutRight");
+          $('.navbar-nav li a').addClass("fadeInRight");
+    }
    lastScrollTop = st;
 }, false);
-};
 
+function ranpos() {
+  var height, width, ranheight,ranwidth, header, span, newelement, newsp,  i;
+    header = $('.header');
+    height = $(window).height();
+    width = $(window).width();
+    randomheight = Math.floor((Math.random() * height) + 1);
+    randomwidth = Math.floor((Math.random() * width) + 1);
+
+      newelement = $('<span><i class="fa fa-bolt fa-4x"</i></span>');
+      header.append(newelement);
+}
+
+}
 $(document).ready(main);
